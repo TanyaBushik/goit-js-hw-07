fc6d26ec76fd753dbb8f7724084d93ac8582ab70
TanyaBushik <tanya.bushik@gmail.com>
2023-03-22 22:41:55 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector(".gallery");
+
+const imageItem = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  )
+  .join("");
+
+galleryListEl.insertAdjacentHTML("beforeend", imageItem);
+
+galleryListEl.addEventListener('click', onImageClick); 
+  
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.NodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+}
+
+
+
+
+
+
+
