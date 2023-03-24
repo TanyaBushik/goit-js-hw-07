c60b1db0c3d4def9bac4bbc86683b7be539ea4e3
TanyaBushik <tanya.bushik@gmail.com>
2023-03-24 23:44:01 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}"
+      alt="${description}" />
+   </a>
+</li>`
+).join('');
+
+galleryListEl.innerHTML = galleryMarkup;
+
+galleryListEl.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    const gallery = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionPosition: "bottom",
+        captionDelay: 250,
+
+    });
+}
